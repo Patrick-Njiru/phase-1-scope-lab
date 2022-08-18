4c1176025b74db37b608fe723e7a43845bd4e519
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-18 23:20:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+var upperCaseCustomerName = () => {
+    customerName = 'BOB'
+    return customerName
+} 
+
+var bestCustomer
+
+var setBestCustomer = () => {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const LeastFavoriteCustomer = 'Mary'
+
+var changeLeastFavoriteCustomer = () => {
+    LeastFavoriteCustomer = 'must be bob'
+    return LeastFavoriteCustomer
+}
